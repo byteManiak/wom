0c65e1a39199d0c4f19cdcc49cc865d5fa3f55cf
bytemaniak <bytemaniak98@gmail.com>
2024-10-09 16:38:16 +0100

    Prevent vanilla mouse actions

--- src/client/java/com/bytemaniak/wov/mixin/client/MinecraftClientMixin.java
+++ src/client/java/com/bytemaniak/wov/mixin/client/MinecraftClientMixin.java
@@ -5,12 +5,17 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.Perspective;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
+    @Shadow @Final public GameOptions options;
+
     @WrapOperation(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;setPerspective(Lnet/minecraft/client/option/Perspective;)V"))
     private void forceThirdPerson(GameOptions instance, Perspective perspective, Operation<Void> original) {
         original.call(instance, Perspective.THIRD_PERSON_BACK);
@@ -18,4 +23,20 @@ public class MinecraftClientMixin {
 
     @WrapOperation(method = "setScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Mouse;lockCursor()V"))
     private void noLockCursor(Mouse mouse, Operation<Void> original) {}
+
+    @WrapOperation(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;wasPressed()Z"))
+    private boolean preventVanillaActions(KeyBinding key, Operation<Boolean> original) {
+        if (key.equals(options.attackKey) || key.equals(options.useKey) || key.equals(options.pickItemKey))
+            return false;
+
+        return original.call(key);
+    }
+
+    @WrapOperation(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;isPressed()Z"))
+    private boolean preventVanillaActions2(KeyBinding key, Operation<Boolean> original) {
+        if (key.equals(options.attackKey) || key.equals(options.useKey))
+            return false;
+
+        return original.call(key);
+    }
 }
