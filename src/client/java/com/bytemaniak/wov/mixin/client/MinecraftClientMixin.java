01468f388bd642af88af3660464869179f68de39
bytemaniak <bytemaniak98@gmail.com>
2024-10-09 18:04:49 +0100

    Attack targeted entity with hotbar presses

--- src/client/java/com/bytemaniak/wov/mixin/client/MinecraftClientMixin.java
+++ src/client/java/com/bytemaniak/wov/mixin/client/MinecraftClientMixin.java
@@ -1,13 +1,21 @@
 package com.bytemaniak.wov.mixin.client;
 
+import com.bytemaniak.wov.registry.Renderers;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.Perspective;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.Hand;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,8 +24,9 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
     @Shadow @Final public GameOptions options;
-
-    @Shadow protected abstract boolean doAttack();
+    @Shadow @Nullable public ClientPlayerInteractionManager interactionManager;
+    @Shadow @Nullable public ClientPlayerEntity player;
+    @Shadow @Nullable public ClientWorld world;
 
     @WrapOperation(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;setPerspective(Lnet/minecraft/client/option/Perspective;)V"))
     private void forceThirdPerson(GameOptions instance, Perspective perspective, Operation<Void> original) {
@@ -46,6 +55,10 @@ public abstract class MinecraftClientMixin {
     @WrapOperation(method = "handleInputEvents", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerInventory;selectedSlot:I"))
     private void attackOnHotbarPress(PlayerInventory instance, int value, Operation<Void> original) {
         original.call(instance, value);
-        doAttack();
+        Entity focusedEntity = world.getEntityById(Renderers.TARGET_RENDERER.focusedEntityId);
+        if (focusedEntity instanceof LivingEntity) {
+            interactionManager.attackEntity(player, focusedEntity);
+            player.swingHand(Hand.MAIN_HAND);
+        }
     }
 }
